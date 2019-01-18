4d2c77b69f1dc6419a30f3d43d2787e5631b51b0
eivinddh <eivind.d.halderaker@hotmail.com>
2019-01-18 12:13:28 +0100

    Removed python 3.7 requirement and worked on the api

--- api.py
+++ api.py
@@ -1,14 +1,31 @@
+from time import gmtime
+
 from flask import Flask
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
+class Coffee(Resource):
     def get(self):
-        return {'hello': 'world'}
+        return {
+                "time": f"{gmtime()}",
+                "coffee": {
+                    "temperature": 1000,
+                    "cups": 2,
+                    "amount": 1
+                    },
+                "mocca": {
+                    "isPowered": False,
+                    "startedBrewing": "" + gmtime(),
+                    "lostPower": True,
+                    "outages": {
+                        ["" + gmtime(), "" + gmtime()]
+                        }
+                    }
+                }
 
-api.add_resource(HelloWorld, '/')
+api.add_resource(Coffee, '/v1/coffee')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5432, debug=True)
