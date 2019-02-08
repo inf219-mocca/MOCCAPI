63aee4faa5257920845a3d6d660839cda5c5ae94
Sondre Nilsen <nilsen.sondre@gmail.com>
2019-02-08 16:25:48 +0100

    Change outages from a DateTime to a Duration that can be null

--- coffee/models.py
+++ coffee/models.py
@@ -14,7 +14,7 @@ class Mocca(models.Model):
     is_powered = models.BooleanField()
     started_brewing = models.DateTimeField("started brewing")
     lost_power = models.BooleanField()
-    outages = models.DateTimeField()
+    outages = models.DurationField(blank=True, null=True)
 
     def __str__(self):
         return f"{self.is_powered}"
