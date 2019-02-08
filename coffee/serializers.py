fb712b27c860a1223560c37c70550913c3d6c1b5
Sondre Nilsen <nilsen.sondre@gmail.com>
2019-02-08 13:52:05 +0100

    Properly serialize the three different classes

--- coffee/serializers.py
+++ coffee/serializers.py
@@ -1,8 +1,23 @@
 from rest_framework import serializers
-from .models import API
+from .models import API, Coffee, Mocca
+
+
+class CoffeeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Coffee
+        fields = "__all__"
+
+
+class MoccaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Mocca
+        fields = "__all__"
 
 
 class APISerializer(serializers.HyperlinkedModelSerializer):
+    coffee = CoffeeSerializer
+    mocca = MoccaSerializer
+
     class Meta:
         model = API
         fields = ("time", "coffee", "mocca")
