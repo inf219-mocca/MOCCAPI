dddfe7b0bac5ba7f434a5aff6adfa30beaa3647d
Sondre Nilsen <nilsen.sondre@gmail.com>
2019-02-15 18:30:20 +0100

    Update time field to measured_at, add help text to fields
    This fixes #15.

--- coffee/models.py
+++ coffee/models.py
@@ -10,9 +10,13 @@ class Coffee(models.Model):
         (POWER_HEATING, "Heating"),
         (POWER_BREWING, "Brewing"),
     )
-    time = models.DateTimeField("time pulled")
-    temperature = models.FloatField()
-    amount = models.FloatField()
+    measured_at = models.DateTimeField(
+        help_text="Datetime (ISO 8601) when the data was read."
+    )
+    temperature = models.FloatField(help_text="Temperature of the coffee")
+    amount = models.FloatField(
+        help_text="Amount of coffee in the pot, going from 0 (empty) to 1 (full)."
+    )
     is_powered = models.PositiveSmallIntegerField(
         choices=POWER_STATUS,
         default=POWER_OFF,
@@ -21,11 +25,17 @@ class Coffee(models.Model):
     1. The machine is keeping the coffee hot.
     2. The machine is brewing coffee.""",
     )
-    started_brewing = models.DateTimeField("started brewing")
-    outages = models.DurationField(blank=True, null=True)
+    started_brewing = models.DateTimeField(
+        help_text="Datetime (ISO 8601) when the current brew was started."
+    )
+    outages = models.DurationField(
+        blank=True,
+        null=True,
+        help_text="Total duration of time when the brewer was without power. Null if no outages was detected.",
+    )
 
     class Meta:
         get_latest_by = "time"
 
     def __str__(self):
-        return f"{self.time}: {self.amount} at {self.temperature}C"
+        return f"{self.measured_at}: {self.amount} at {self.temperature}C"
