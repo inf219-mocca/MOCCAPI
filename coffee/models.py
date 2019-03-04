0f00ae57432253f25a81702d4f565a5df0e6dd33
Sondre Nilsen <nilsen.sondre@gmail.com>
2019-03-04 12:55:39 +0100

    Fix ordering being last -> newest, show 25 readings in API

--- coffee/models.py
+++ coffee/models.py
@@ -36,6 +36,7 @@ class Coffee(models.Model):
 
     class Meta:
         get_latest_by = "measured_at"
+        ordering = ("-measured_at",)
 
     def __str__(self):
         return f"{self.measured_at}: {self.amount} at {self.temperature}C"
