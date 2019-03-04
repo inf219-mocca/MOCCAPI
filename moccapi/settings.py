2e938dcd6d76ca01f11ffed0a33e9183fb5b462c
Sondre Nilsen <nilsen.sondre@gmail.com>
2019-03-04 13:12:52 +0100

    Set timezone to Europe/Oslo

--- moccapi/settings.py
+++ moccapi/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
 LANGUAGE_CODE = "en-us"
-TIME_ZONE = "UTC"
+TIME_ZONE = "Europe/Oslo"
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
