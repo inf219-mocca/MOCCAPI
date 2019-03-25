1d503a4b327b951bffbab42fb215819c7df318a9
Sondre Nilsen <nilsen.sondre@gmail.com>
2019-03-25 18:12:21 +0100

    Add get_brew method, auto add started_brewing parametre, add meta class

--- brew/models.py
+++ brew/models.py
@@ -3,13 +3,27 @@ from django.db import models
 
 class Brew(models.Model):
     started_brewing = models.DateTimeField(
-        help_text="Datetime (ISO 8601) when the current brew was started."
+        auto_now_add=True,
+        help_text="Datetime (ISO 8601) when the current brew was started.",
     )
     outages = models.DurationField(
-        blank=True,
-        null=True,
-        help_text="Total duration of time when the brewer was without power. Null if no outages was detected.",
+        help_text="Total duration of time when the brewer was without power. Null if no outages was detected."
     )
 
+    class Meta:
+        get_latest_by = "started_brewing"
+        ordering = ("-started_brewing",)
+
     def __str__(self):
-        return f"Brew {self.id}: {self.started_brewing} - {self.outages}"
+        return f"ID: {self.id}\nStarted brewing: {self.started_brewing}\nOutages: {self.outages}"
+
+
+def get_brew(power: int) -> Brew:
+    from coffee.models import Coffee
+
+    if power > 1000:
+        return Brew.objects.latest()
+    elif power < 1000 and Coffee.objects.latest().is_powered > 2:
+        return Brew()
+    else:
+        raise Exception("This shouldn't happen...")
