8db92499a07fc923d77630b37b9dcb562df94e02
Sondre Nilsen <nilsen.sondre@gmail.com>
2019-05-03 15:40:46 +0200

    Run migrations on production release

--- entry.sh
+++ entry.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 echo "Starting moccapi in production environment"
+pipenv run python manage.py migrate
 pipenv run gunicorn moccapi.wsgi:application --access-logfile - --bind localhost:8000 --workers 3
