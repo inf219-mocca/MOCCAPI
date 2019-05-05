c26d62e1b99e30334a259e17e9459ffd7b7c57ae
Sondre Nilsen <nilsen.sondre@gmail.com>
2019-05-05 16:16:19 +0200

    Update entry.sh to work on rPi [ci skip]

--- entry.sh
+++ entry.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
 echo "Starting moccapi in production environment"
-pipenv run python manage.py migrate
-pipenv run gunicorn moccapi.wsgi:application --access-logfile - --bind localhost:8000 --workers 3
+pipenv run gunicorn moccapi.wsgi:application --access-logfile - --bind 0.0.0.0:8000 --workers 3
