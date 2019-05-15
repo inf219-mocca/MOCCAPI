91b5e2a3357612c69a7579962a43e6bbb3d6fdb8
Sondre Nilsen <nilsen.sondre@gmail.com>
2019-05-15 19:57:31 +0200

    Add dependencies, make camera functions static

--- sensors/camera.py
+++ sensors/camera.py
@@ -8,13 +8,15 @@ from PIL import Image
 
 
 class Camera:
-    def convert_image(self, img_array, threshold: int, mean: int):
+    @staticmethod
+    def convert_image(img_array, threshold: int, mean: float):
         """Convert image into a binary representation of black/white."""
         arr = (img_array > threshold).mean(1)
         arr = arr > mean
         return arr
 
-    def take_picture(self):
+    @staticmethod
+    def take_picture():
         """Take a picture with the default Pi camera."""
         stream = BytesIO()
 
@@ -29,26 +31,16 @@ class Camera:
         arr = array(image)
         return arr
 
-    def write_picture(self, name: str, image):
+    @staticmethod
+    def write_picture(name: str, image):
         """Write a picture to a file."""
         imwrite(name, image)
 
-    def write_binary_picture(self, image, name: str, height: int):
+    @staticmethod
+    def write_binary_picture(image, name: str, height: int):
         """Create and write a binary (black/white only) image."""
         out = Image.new("1", (height, 1))
         pixels = out.load()
         for (i, x) in enumerate(image):
             pixels[i, 0] = 1 if x else 0
         out.save(name)
-
-
-c = Camera()
-image = c.take_picture()
-c.write_picture("pi.png", image)
-c.write_binary_picture(c.convert_image(image, 80, 0.5), "stripe.png", 360)
-
-oldImage = Image.open("Original.png")
-oldImage = oldImage.convert("L")
-oldImage = array(oldImage)
-c.write_picture("Original2.png", oldImage)
-c.write_binary_picture(c.convert_image(oldImage, 80, 0.5), "stripeOld.png", 360)
